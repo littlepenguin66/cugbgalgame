44bd04eb5e59cead9bf8ae1568a921c758cb9a66
littlepenguin66 <littlepenguinzq@gmail.com>
2024-04-08 12:41:18 +0800

    update 5 files

--- data_preproccessing/preproccessing.py
+++ data_preproccessing/preproccessing.py
@@ -5,27 +5,31 @@ from multiprocessing import Pool
 import itertools
 
 def output_filename(file_name):
+    # 生成输出文件名，在原文件名后添加'_cutted'
     basename, ext = os.path.splitext(file_name)
     return basename + '_cutted' + ext
 
 def segment(text, column_name):
+    # 对文本进行分词处理，使用空格拼接分词结果
     return ' '.join(jieba.cut(text))
 
 def process_row(args):
+    # 处理每一行数据，为指定列进行分词，创建新列'分词结果'
     row_tuple, column_name = args
     index, row = row_tuple
     row['分词结果'] = segment(row[column_name], column_name)
     return row[['分词结果']]
 
 def main():
-    # 从 file_name.txt 中读取多个 CSV 文件名
+    # 主函数
     try:
+        # 从file_name.txt中读取多个CSV文件名
         with open('../Crawlers/Data/file_name.txt', 'r') as file:
             file_names = file.read().splitlines()  # 按行分割
     except FileNotFoundError:
-        # 如果文件不存在，创建 file_name.txt 并退出程序
+        # 如果文件不存在，创建file_name.txt并退出程序
         with open('../Crawlers/Data/file_name.txt', 'w') as file:
-            print("请在 file_name.txt 中提供 CSV 文件名。")
+            print("请在file_name.txt中提供CSV文件名。")
             exit()
 
     for file_name in file_names:
