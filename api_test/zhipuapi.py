cc4c36dc92651dd4f8248d109a170d380cec08c8
littlepenguin66 <littlepenguinzq@gmail.com>
2024-04-09 14:31:25 +0800

    reload zhipuapi.py

--- api_test/zhipuapi.py
+++ api_test/zhipuapi.py
@@ -1,3 +1,4 @@
+import json
 from zhipuai import ZhipuAI
 
 client = ZhipuAI(api_key="83edf8e74ea585562cc46886023c6086.FRnRwwCmiF05NyO2") # 填写您自己的APIKey
@@ -9,11 +10,14 @@ with open ("text.txt", "r", encoding="utf-8") as f:
 response = client.chat.completions.create(
     model="glm-4",  # 填写需要调用的模型名称
     messages=[
-        {"role": "assistant", "content": "你好，我是一个自动文本生成机器人，我可以帮助你将文本转化为指定格式。"},
-        {"role": "user","content": "务必记住content是文章所描述的具体对象，summary是对该对象的描述，若有多个具体对象则相应生成多对content与summary，每一组均有一对{}"},
-        {"role": "user","content": "content的内容应尽可能精确，避免出现多个对象的描述，summary的内容应尽可能简洁，避免过于冗长。"},
+        {"role": "assistant", "content": "你好，我是一个自动文本生成机器人，我可以帮助你将提供的内容转化为指定格式。只要输出代码即可，不需要其他。"},
+        {"role": "user","content": "务必记住content是文章所描述的具体对象，summary是对该对象的描述，若有多个具体对象则相应生成多对content与summary，每一组均有一对{},只要输出代码即可，不需要其他。"},
+        {"role": "user","content": "content的内容应尽可能精确，避免出现多个对象的描述,只要输出代码即可，不需要其他。"},
         {"role": "user","content": '请你将以下这段文字转化为若干{"content": "","summary": ""}的格式，content内容为文章描述的具体对象，summary为对该对象的描述,若有多个具体对象则相应生成多对content与summary，每一组均有一对{}' + text},
     ],
-    temperature=0.4,
+    temperature=0.8,
 )
-print(response.choices[0].message.content)
+data = response.choices[0].message.content
+
+with open("output.json", "a", encoding="utf-8") as f:
+    f.write(data)
